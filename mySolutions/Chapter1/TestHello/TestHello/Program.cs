ddf1a96bd49a638c30df992b77ba95a46a75fabd
tshavor <thomasshavor@gmail.com>
2017-01-18 05:46:27 -0600

    exercise one started

---
+++ mySolutions/Chapter1/TestHello/TestHello/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHello             //name of our project, also becomes the top level solution file in the Solution Explorer window to the right
+{
+    class Program
+    {
+        static void Main(string[] args)      //this is the starting point for the app.  It should also be "static", which means the METHOD DOES NOT return a value
+        {
+            Console.WriteLine("Hello World!");  //Console.Writeline is exactly like "Console.log"
+
+        }
+    }
+}
