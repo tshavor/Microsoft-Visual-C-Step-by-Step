5a0490c1964a2b0b6f19e48aa6029df5d26e443d
tshavor <thomasshavor@gmail.com>
2017-01-18 09:57:35 -0600

    program completed

--- mySolutions/Chapter1/Hello/Hello/MainPage.xaml.cs
+++ mySolutions/Chapter1/Hello/Hello/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +26,14 @@ namespace Hello
         public MainPage()
         {
             this.InitializeComponent();  //an example of a CONSTRUCTOR. "this" refers to the same name as the class (i.e. "MainPage")
+                                         // A CONSTRUCTOR is a special method with the same name as the class.
+                                         //it runs when an instance of the class is created, and can contain code to initialize the instance.
+        }
+
+        private void okClick(object sender, RoutedEventArgs e)
+        {
+            MessageDialog msg = new MessageDialog("Hello" + " " + userName.Text);
+            msg.ShowAsync();
         }
     }
 }
